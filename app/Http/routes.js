36596e95247816596d3017aec1428fc93ddbdb9e
Ryan Tablada <ryan.tablada@gmail.com>
2017-04-26 14:20:35 -0500

    CRM done

--- app/Http/routes.js
+++ app/Http/routes.js
@@ -23,23 +23,53 @@ const Message = use('App/Model/Message')
 const User = use('App/Model/User')
 const Hash = use('Hash')
 
-Route.get('/login', function *(request, response) {
+Route.get('/login', function* (request, response) {
   yield response.sendView('login')
 })
 
-Route.post('/login', function *(request, response) {
-  const { email, password } = request.only('email', 'password')
+Route.post('/login', function* (request, response) {
+  const {
+    email,
+    password
+  } = request.only('email', 'password')
 
   const login = yield request.auth.attempt(email, password)
 
-    if (login) {
-      return response.redirect('/crm/messages')
-    }
+  if (login) {
+    return response.redirect('/crm/messages')
+  }
 
-    response.unauthorized('Invalid credentails')
+  response.unauthorized('Invalid credentails')
 })
 
-Route.post('/messages', function *(request, response) {
+Route.group('crm', function () {
+  Route.get('/crm/messages', function* (request, response) {
+    const messages = yield Message.all()
+
+    yield response.sendView('message.index', {
+      messages: messages.toJSON()
+    })
+  })
+
+  Route.get('/crm/messages/:id', function* (request, response) {
+    const message = yield Message.find(request.param('id'))
+
+    yield response.sendView('message.show', {
+      message: message.toJSON()
+    })
+  })
+
+  Route.post('/crm/messages/:id/contacted', function* (request, response) {
+    const message = yield Message.find(request.param('id'))
+    message.contacted = JSON.parse(request.input('contacted'))
+
+    yield message.save()
+
+    yield response.redirect('back')
+  })
+}).middleware('auth')
+
+Route.post('/messages', function* (request, response) {
   const params = request.only('email', 'name', 'message')
 
   const m = yield Message.create(params)
